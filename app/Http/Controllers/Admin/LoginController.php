b0c35d9029d6b1fac333c8db63d92d92f5e08b51
Santosh Dash <santoshnet2016@gmail.com>
2019-05-05 07:46:26 -0700

    Multi Auth Added

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('logout');
+    }
+
+    public function showLoginForm(){
+        return view('admin.login');
+    }
+    
+    public function login(Request $request){
+        
+        $this->validate($request, [
+           'email' => 'required|email',
+           'password' => 'required|min:6'
+        ]);
+
+
+       if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password],$request->remember)){
+            return redirect()->intended(route('admin.dashboard'));
+       }
+
+       return redirect()->back()->withInput($request->only('email','remember'));
+    }
+    
+
+
+   
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+
+
+Route::prefix('admin')->group(function(){
+  
+   Route::get('/login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+   Route::post('/login', 'Admin\LoginController@login')->name('admin.login.submit');
+   Route::get('/', 'Admin\AdminController@index')->name('admin.dashboard');
+
+});
